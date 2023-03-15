4a0c0f8c7713ecef6811367435af9b01495b7e2a
Ali Baghernejad <alibaghernezhad@gmail.com>
2023-03-15 09:13:58 +0330

    Show the result of the search  query in ulauncher.

--- main.py
+++ main.py
@@ -1,12 +1,17 @@
 import urllib.request, urllib.parse, urllib.error
 import logging
+import webbrowser
+from bs4 import BeautifulSoup
+import requests
 
 from ulauncher.api.client.Extension import Extension
 from ulauncher.api.client.EventListener import EventListener
 from ulauncher.api.shared.event import KeywordQueryEvent, ItemEnterEvent
 from ulauncher.api.shared.item.ExtensionResultItem import ExtensionResultItem
+from ulauncher.api.shared.action.ExtensionCustomAction import ExtensionCustomAction
 from ulauncher.api.shared.action.RenderResultListAction import RenderResultListAction
 from ulauncher.api.shared.action.OpenUrlAction import OpenUrlAction
+from ulauncher.api.shared.action.HideWindowAction import HideWindowAction
 
 LOGGER = logging.getLogger(__name__)
 
@@ -21,26 +26,70 @@ class PonsExtension(Extension):
     def __init__(self):
         super(PonsExtension, self).__init__()
         self.subscribe(KeywordQueryEvent, KeywordQueryEventListener())
+        self.subscribe(ItemEnterEvent, ItemEnterEventListener())
+
 
 class KeywordQueryEventListener(EventListener):
 
     def on_event(self, event, extension):
         items = list()
-
-        if event.get_argument():
+        
+        query= event.get_argument()
+        if query:
             LOGGER.info('Word Pons search for "{}"'.format(event.get_argument()))
-            items.append(
-                ExtensionResultItem(
-                    icon='images/icon.png',
-                    name='Define words on Pons',
-                    description='Define words "{}".'.format(event.get_argument()),
-                    on_enter=OpenUrlAction(
-                        'https://en.pons.com/translate/' + extension.preferences["source"] + "-" + extension.preferences["target"] + '/{}'.format(url_safe_chunk(event.get_argument())) + '?{}'.format(urlencode({ 'bidir': '1' }))
-                    )
-                )
-            )
+
+            address = f'https://en.pons.com/translate/' + extension.preferences["source"] + "-" + extension.preferences["target"] + '/{}'.format(url_safe_chunk(event.get_argument())) + '?{}'.format(urlencode({ 'bidir': '1' }))
+            request = requests.get(address)
+            response = BeautifulSoup(request.content, 'html.parser')
+            
+            try:
+                translate = ','.join(i.dt.text.strip()+ ' -> ' + i.dd.text.strip() + '\n' for i in response.find('div', class_='results').find_all('dl', {'class': 'dl-horizontal'})[:3])
+            except:
+                translate = 'Not found :('            
+
+            data = {
+                'name': query,
+                'description': translate,
+                'address': address
+            }
+
+            return RenderResultListAction([
+                ExtensionResultItem(icon='images/icon.png',
+                    name=data['name'],
+                    description=data['description'],
+                    on_enter=ExtensionCustomAction(data, keep_app_open=True))
+            ])
+        
+            # items.append(
+            #     ExtensionResultItem(
+            #         icon='images/icon.png',
+            #         name='Define words on Pons',
+            #         description='Define words "{}".'.format(event.get_argument()),
+            #         on_enter=OpenUrlAction(
+            #             'https://en.pons.com/translate/' + extension.preferences["source"] + "-" + extension.preferences["target"] + '/{}'.format(url_safe_chunk(event.get_argument())) + '?{}'.format(urlencode({ 'bidir': '1' }))
+            #         )
+            #     )
+            # )
+
+        else:
+            return RenderResultListAction([
+                ExtensionResultItem(icon='images/icon.png',
+                    name='Search Pons',
+                    description='Search for a word in Pons',
+                    on_enter=HideWindowAction())
+            ])
 
         return RenderResultListAction(items)
 
+class ItemEnterEventListener(EventListener):
+    def on_event(self, event, extension):
+        data = event.get_data()
+        return RenderResultListAction([
+            ExtensionResultItem(icon='images/icon.png',
+                                name=data['name'],
+                                description=data['description'],
+                                on_enter=webbrowser.open(data['address']))
+        ])
+    
 if __name__ == '__main__':
     PonsExtension().run()
